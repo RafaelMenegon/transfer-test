1a6da4b3f8c8b892f300c3c54f9ef04aaaa2ba17
RafaelMenegon <rafamenerafamene@gmail.com>
2024-06-22 12:52:39 -0300

    configuracao do banco de dados

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Transfer Project!");
+        System.out.println("Configuração do banco de dados");
     }
 }
